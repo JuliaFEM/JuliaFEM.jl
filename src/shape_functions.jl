25f5fe541e5264133c1d6a1a542a03223eef49bb
Olli Vainola <olli.vainola@gbw.fi>
2015-05-16 10:04:27 +0300

    hexa shape functions added

--- src/shape_functions.jl
+++ src/shape_functions.jl
@@ -1,1 +1,19 @@
-function rectangle_linear(\eta, \zeta)
+function C2D4(ξ::Float64, η::Float64)
+     values::Array{Float64, 1} = [(1 - ξ) * (1 - η),
+                                  ξ * (1 - η),
+                                  ξ * η,
+                                  (1 - ξ) * η]
+    return values
+end
+
+function C3D8(ξ::Float64, η::Float64, μ::Float64)
+    values::Array{Float64, 1} = 1 / 8 * [(1 - ξ) * (1 - η) * (1 - μ),
+                                         (1 + ξ) * (1 - η) * (1 - μ),
+                                         (1 + ξ) * (1 + η) * (1 - μ),
+                                         (1 - ξ) * (1 + η) * (1 - μ),
+                                         (1 - ξ) * (1 - η) * (1 + μ),
+                                         (1 + ξ) * (1 - η) * (1 + μ),
+                                         (1 + ξ) * (1 + η) * (1 + μ),
+                                         (1 - ξ) * (1 + η) * (1 + μ)]
+    return values
+end
