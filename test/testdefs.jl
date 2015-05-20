5b75428e04f1d19061bf68cc6790fc32f4e4d032
Tero Frondelius <tero.frondelius@gmail.com>
2015-05-20 20:34:37 +0300

    initial commit

---
+++ test/testdefs.jl
@@ -0,0 +1,23 @@
+# This file is a part of Julia. License is MIT: http://julialang.org/license
+
+using Base.Test
+
+function runtests(name)
+    @printf("     \033[1m*\033[0m \033[31m%-20s\033[0m", name)
+    tt = @elapsed Core.include(abspath("$name.jl"))
+    @printf(" in %6.2f seconds\n", tt)
+    nothing
+end
+
+function propagate_errors(a,b)
+    if isa(a,Exception)
+        rethrow(a)
+    end
+    if isa(b,Exception)
+        rethrow(b)
+    end
+    nothing
+end
+
+# looking in . messes things up badly
+filter!(x->x!=".", LOAD_PATH)
---
+++ test/testing_testing.jl
@@ -0,0 +1,5 @@
+# This file is a part of JuliaFEM. License is MIT: https://github.com/ovainola/JuliaFEM/blob/master/README.md
+
+using Base.Test
+
+@test 1 == 1
