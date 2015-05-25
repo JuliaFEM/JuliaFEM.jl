868a9d2f8ff52d8b09df3a537a49acbd676f44c3
Tero Frondelius <tero.frondelius@gmail.com>
2015-05-25 22:39:30 +0300

    travis CI updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,8 @@
-# This file is a part of Julia. License is MIT: http://julialang.org/license
+using JuliaFEM
+using Base.Test
 
-include("choosetests.jl")
-tests, net_on = choosetests(ARGS)
-cd(dirname(@__FILE__)) do
-    n = 1
-    if net_on
-        n = min(8, CPU_CORES, length(tests))
-        n > 1 && addprocs(n; exeflags=`--check-bounds=yes`)
-        blas_set_num_threads(1)
-    end
+# write your own tests here
+@test 1 == 1
 
-    @everywhere include("testdefs.jl")
 
-    reduce(propagate_errors, nothing, pmap(runtests, tests; err_retry=false, err_stop=true))
-
-    @unix_only n > 1 && rmprocs(workers(), waitfor=5.0)
-    println("    \033[32;1mSUCCESS\033[0m")
-end
+@test_approx_eq 1.0 1.0
