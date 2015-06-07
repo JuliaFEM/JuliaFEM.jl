0c55e1bfbaaa21e5397855abbe25da2340a3a679
ovainola <olli.vainola@student.oulu.fi>
2015-06-07 13:43:01 +0300

    Adding C2D4 shape function

--- src/shape_functions.jl
+++ src/shape_functions.jl
@@ -1,3 +1,14 @@
 function test(a)
      return a*2
 end
+
+
+function C2D4(xx)
+     chi = xx[1]
+     eta = xx[2]
+     return = [(1 - chi) * (1 - eta),
+               chi * (1 - eta),
+               chi * eta,
+               (1 - chi) * eta]
+end
+
