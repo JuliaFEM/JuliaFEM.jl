2f541ea4afba3a188ff19e6fb82de0439e6827e7
Tero Frondelius <tero.frondelius@gmail.com>
2015-06-23 23:44:00 +0300

    removed old and obsolete files

--- test/testing_testing.jl
+++
@@ -1,8 +0,0 @@
-# This file is a part of JuliaFEM. License is MIT: https://github.com/ovainola/JuliaFEM/blob/master/README.md
-
-using JuliaFEM
-using Base.Test
-
-@test 1 == 1
-
-@test_approx_eq 1.0 1.0
