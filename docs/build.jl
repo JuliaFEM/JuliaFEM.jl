63fa27ce25ca5c15d7b01cf3f88d7a3db2a95b2f
ovainola <olli.vainola@student.oulu.fi>
2015-06-26 20:10:47 +0300

    Update build.jl
    Added doctests to build.jl

--- docs/build.jl
+++ docs/build.jl
@@ -10,6 +10,15 @@ const modules = [JuliaFEM, JuliaFEM.elasticity_solver]
 # run(`cp $main_folder/$file_ $this_folder`)
 
 cd(dirname(@__FILE__)) do
+    # Run the doctests *before* we start to generate *any* documentation.
+    for m in modules
+        failures = failed(doctest(m))
+        if !isempty(failures.results)
+            println("\nDoctests failed, aborting commit.\n")
+            display(failures)
+            exit(1) # Bail when doctests fail.
+        end
+    end
     # Generate and save the contents of docstrings as markdown files.
     index  = Index()
     for mod in modules
