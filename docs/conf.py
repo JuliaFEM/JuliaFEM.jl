edac8acdd17670b1a7201fad51a81792e0d9024c
ovainola <olli.vainola@student.oulu.fi>
2015-06-26 22:53:26 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,9 @@
 import juliadoc
+import os
 
 extensions = ['juliadoc.julia', 'juliadoc.jlhelp']
 html_theme_path = [juliadoc.get_theme_dir()]
 html_sidebars = juliadoc.default_sidebars()
+
+print(os.getcwd())
+os.system("julia build.jl")
