955d280cb84616312466a4d71898da5afe6c1684
Jukka Aho <ahojukka5@gmail.com>
2015-06-27 18:46:07 +0300

    add test to throw error when element type is unknown

--- test/test_abaqus_reader.jl
+++ test/test_abaqus_reader.jl
@@ -5,7 +5,7 @@ using FactCheck
 using Logging
 @Logging.configure(level=INFO)
 
-using JuliaFEM.abaqus_reader: parse_abaqus
+using JuliaFEM.abaqus_reader: parse_abaqus, parse_element_section
 
 facts("test import abaqus model") do
     fid = open("../geometry/3d_beam/palkki.inp")
@@ -17,4 +17,15 @@ facts("test import abaqus model") do
     @fact length(model["nsets"]["SUPPORT"]) => 9
     @fact length(model["nsets"]["LOAD"]) => 9
     @fact length(model["nsets"]["TOP"]) => 83
+end
+
+facts("test that reader throws error when dimension information of elemenet is missing") do
+#   *ELEMENT, TYPE=neverseenbefore, ELSET=Body1
+  data = """
+         1,       243,       240,       191,       117,       245,       242,       244,
+         1,         2,       196
+  """
+  model = Dict()
+  header = Dict("section"=>"ELEMENT", "options" => Dict("TYPE" => "neverseenbefore", "ELSET"=>"Body1"))
+  @fact_throws parse_element_section(model, header, data)
 end
