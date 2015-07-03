03f7caff772673af36e2e68bc2504e71aa004fe7
Jukka Aho <ahojukka5@gmail.com>
2015-07-03 23:38:17 +0300

    added extensions, removed code i do not understand

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ html_sidebars = juliadoc.default_sidebars()
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+#extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
