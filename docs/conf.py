d4d0c657b9c8513d36f5ee1c6bcaf22247ea18de
Jukka Aho <ahojukka5@gmail.com>
2015-07-04 04:19:26 +0300

    added .rst to known documentation

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.md'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
