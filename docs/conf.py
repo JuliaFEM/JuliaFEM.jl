0628286eab1ddc525dc96ee4fc43aa84165f3da0
Jukka Aho <ahojukka5@gmail.com>
2015-07-06 22:30:35 +0300

    caused a bug. fixed

--- docs/conf.py
+++ docs/conf.py
@@ -143,10 +143,10 @@ html_show_copyright = False
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = JuliaFEM - A Finite Element Method Solver
+html_title = "JuliaFEM - A Finite Element Method Solver"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = JuliaFEM
+html_short_title = "JuliaFEM"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
