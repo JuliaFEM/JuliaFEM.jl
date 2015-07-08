d561f5f75d2097769c76c323b6386d1fae63c40b
Jukka Aho <ahojukka5@gmail.com>
2015-07-08 23:47:29 +0300

    logged output

--- docs/build_unittests.jl
+++ docs/build_unittests.jl
@@ -3,8 +3,6 @@ import Logging
 include("badges.jl")
 
 cd(dirname(@__FILE__)) do
-    # found easier way for this .. maybe
-    # Logging.configure(output=open("quality/unittests_report.rst", "w"))
     include("../test/runtests.jl")
     s = FactCheck.getstats()
     make_badge("unittests", s["nSuccesses"], sum(values(s)), "badges/unittests-status.svg")
