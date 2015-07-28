0876c9a543ef8101caf519e554a55ac982cc7bbc
Jukka Aho <ahojukka5@gmail.com>
2015-07-28 16:55:40 +0300

    added logo to sphinx configuratoin

--- docs/conf.py
+++ docs/conf.py
@@ -150,12 +150,12 @@ html_short_title = "JuliaFEM"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = './logo/JuliaFEMLogo_96x96.png'
+html_logo = 'logo/JuliaFEMLogo_96x96.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'logo/JuliaFEMLogo_32x32.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
