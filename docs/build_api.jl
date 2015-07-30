cc54c626add0017f059f26ea7f24ea589750432f
ovainola <olli.vainola@student.oulu.fi>
2015-07-30 20:41:52 +0300

    backup

--- docs/build_api.jl
+++ docs/build_api.jl
@@ -42,28 +42,28 @@ const modules = append_list
 
 cd(dirname(@__FILE__)) do
     # Run the doctests *before* we start to generate *any* documentation.
-    for m in modules
-        failures = failed(doctest(m))
-        if !isempty(failures.results)
-            println("\nDoctests failed, aborting commit.\n")
-            display(failures)
-            exit(1) # Bail when doctests fail.
-        end
-    end
+#    for m in modules
+#        failures = failed(doctest(m))
+#        if !isempty(failures.results)
+#            println("\nDoctests failed, aborting commit.\n")
+#            display(failures)
+#            exit(1) # Bail when doctests fail.
+#        end
+#    end
     # Generate and save the contents of docstrings as markdown files.
     index  = Index()
     for mod in modules
-        Lexicon.update!(index, save(joinpath(api_directory, "$(mod).md"), mod))
+        Lexicon.update!(index, save(joinpath(api_directory, "$(mod).rst"), mod))
     end
-    save(joinpath(api_directory, "index.md"), index; md_subheader = :category)
+    #save(joinpath(api_directory, "index.rst"))
 
     # Add a reminder not to edit the generated files.
-    open(joinpath(api_directory, "README.md"), "w") do f
-        print(f, """
-        Files in this directory are generated using the `build.jl` script. Make
-        all changes to the originating docstrings/files rather than these ones.
-        """)
-    end
+  #  open(joinpath(api_directory, "README.md"), "w") do f
+  #      print(f, """
+  #      Files in this directory are generated using the `build.jl` script. Make
+  #      all changes to the originating docstrings/files rather than these ones.
+  #      """)
+  #  end
 
     # info("Adding all documentation changes in $(api_directory) to this commit.")
     # success(`git add $(api_directory)`) || exit(1)
