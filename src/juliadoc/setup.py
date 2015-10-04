abb82684ec119373ddbd9dfa3293b07e0e8accac
Olli Väinölä <olli.vainola@student.oulu.fi>
2015-10-04 13:44:55 +0300

    Deleted src/juliadoc & modified von mises

--- src/juliadoc/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='JuliaDoc',
-    version='0.0.0',
-    packages=['juliadoc',],
-    license='MIT',
-    long_description=open('README.md').read(),
-)
