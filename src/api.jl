c71c40b952a788ad468f37598a0d27090ae7b7ac
Olli Väinölä <olli.vainola@student.oulu.fi>
2015-11-28 17:03:48 +0200

    Abaqus_reader: can read nodes, elements and sets

--- src/api.jl
+++ src/api.jl
@@ -1,6 +1,12 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
 
+abstract calculation_model
+
+type Model <: calculation_model
+    jotain
+end
+
 
 type INode{T<:Real}
     id::Integer
@@ -11,14 +17,18 @@ type IElement{T<:Real}
     id::Integer
     connectivity::Array{T, 1}
     element_type::AbstractString
+    # fields :: Dict{AbstractString, Field}
 end
 
+# IElement(id, conn, eltype) = IElement(id, conn, eltype, Dict())
 
-type INodeset
-    node_ids
+type INodeSet
+    name::AbstractString
+    node_ids::Array{Integer, 1} 
 end
 
-type IElementset
-    jotain
+type IElementSet
+    name::AbstractString
+    element_ids::Array{Integer, 1}
 end
 
