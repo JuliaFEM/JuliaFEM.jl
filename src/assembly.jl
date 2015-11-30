92f52e34e7a01f48646e2dc91e6f49d03e543b5b
Jukka Aho <jukka.aho@kapsi.fi>
2015-11-30 16:04:13 +0200

    substructuring, fixed tests, possibility to save to integration points

--- src/assembly.jl
+++ src/assembly.jl
@@ -3,6 +3,16 @@
 
 # Functions to handle global assembly of problem
 
+type CAssembly
+    interior_dofs :: Vector{Int}
+    boundary_dofs :: Vector{Int}
+    F :: Factorization
+    Kc :: SparseMatrixCSC
+    fc :: SparseMatrixCSC
+    Ki :: SparseMatrixCSC
+    fi :: SparseMatrixCSC
+end
+
 function assemble!(assembly::Assembly, problem::AllProblems, time::Float64, empty_assembly::Bool=true)
     if empty_assembly
         empty!(assembly)
@@ -18,6 +28,73 @@ function assemble(problem::AllProblems, time::Float64)
         assemble!(assembly, problem, element, time)
     end
     return assembly
+end
+
+""" Return condensed system. """
+function assemble(problem::FieldProblem, time::Float64, boundary_dofs::Vector{Int})
+    assembly = Assembly()
+    for element in get_elements(problem)
+        assemble!(assembly, problem, element, time)
+    end
+    return condensate(assembly, boundary_dofs)
+end
+
+function condensate(assembly::Assembly, boundary_dofs_::Vector{Int})
+    K = sparse(assembly.stiffness_matrix)
+    all_dofs = unique(assembly.stiffness_matrix.I)
+    boundary_dofs = intersect(all_dofs, boundary_dofs_)
+    interior_dofs = setdiff(all_dofs, boundary_dofs_)
+    dim = size(K, 1)
+    f = sparse(assembly.force_vector, dim, 1)
+
+    # check that matrix is symmetric
+    asdf = maximum(abs(1/2*(K + K') - K))
+    if asdf > 1.0e-6
+        info(full(K))
+        error("asdf $asdf > 1.0e-6")
+    end
+    
+    K = 1/2*(K + K')
+    F::Factorization = cholfact(K[interior_dofs, interior_dofs])
+
+#   info("condensation: all dofs: ", all_dofs)
+#   info("condensation: interior dofs: ", interior_dofs)
+#   info("condensation: boundary dofs: ", boundary_dofs)
+#   info("manually condensated")
+#   Kman = K[boundary_dofs, boundary_dofs] - K[boundary_dofs,interior_dofs] * inv(full(K[interior_dofs, interior_dofs])) * K[interior_dofs, boundary_dofs]
+#   info("\n$(full(Kman))")
+    #info("K = \n$(full(K))")
+    #Ki = K[interior_dofs, boundary_dofs]
+    Ki = K[interior_dofs, boundary_dofs]
+    fi = f[interior_dofs]
+#   info("condensated using factorization")
+#   LL = K[boundary_dofs, boundary_dofs] - K[boundary_dofs, interior_dofs] * (K[interior_dofs, interior_dofs] \ K[interior_dofs, boundary_dofs])
+#   info(LL)
+
+    Ks = F \ Ki
+    Fs = F \ fi
+
+    dim = size(K, 1)
+    Kc = spzeros(dim, dim)
+    fc = spzeros(dim, 1)
+    Kc[boundary_dofs, boundary_dofs] = K[boundary_dofs, boundary_dofs] - Ki' * Ks
+    fc[boundary_dofs] = f[boundary_dofs] - Ki' * Fs
+
+
+    return CAssembly(interior_dofs, boundary_dofs, F, Kc, fc, Ki, fi)
+end
+
+function reconstruct!(ca::CAssembly, x::SparseMatrixCSC)
+#   info("size of la = ", size(la))
+#   info("size of ca.Ki = ", size(ca.Ki))
+#   info("size of ca.fi = ", size(ca.fi))
+#   info("size of la[ca.interior_dofs] = ", size(la[ca.interior_dofs]))
+#   info("interior dofs: $(ca.interior_dofs)")
+#   info("boundary dofs: $(ca.boundary_dofs)")
+#   info("ca.fi = $(ca.fi')")
+#   info("sol1 = ", full(ca.F \ ca.fi)')
+#   info("sol2 = ", full(ca.F \ (ca.Ki*x[ca.boundary_dofs]))')
+    x[ca.interior_dofs] += ca.F \ (ca.fi - ca.Ki*x[ca.boundary_dofs])
 end
 
 function Base.(:+)(ass1::Assembly, ass2::Assembly)
---
+++ test/test_assembly.jl
@@ -0,0 +1,57 @@
+# This file is a part of JuliaFEM.
+# License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
+
+module AssemblyTests
+
+using JuliaFEM
+using JuliaFEM.Test
+using JuliaFEM: Seg2, Quad4, HeatProblem, DirichletProblem, assemble
+using JuliaFEM: condensate, reconstruct!
+
+function test_static_condensation()
+    nodes = Vector[[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]]
+
+    el1 = Quad4([1, 2, 3, 4])
+    el1["geometry"] = Vector[nodes[1], nodes[2], nodes[3], nodes[4]]
+    el1["temperature thermal conductivity"] = 6.0
+    el1["temperature load"] = [12.0, 12.0, 12.0, 12.0]
+    el2 = Seg2([1, 2])
+    el2["geometry"] = Vector[[0.0, 0.0], [1.0, 0.0]]
+    el2["temperature flux"] = 6.0
+    field_problem = HeatProblem()
+    push!(field_problem, el1)
+    push!(field_problem, el1)
+
+    el3 = Seg2([3, 4])
+    el3["geometry"] = Vector[nodes[3], nodes[4]]
+    el3["temperature"] = 0.0
+    boundary_problem = DirichletProblem("temperature", 1)
+    push!(boundary_problem, el3)
+
+    fass = assemble(field_problem, 0.0)
+    bass = assemble(boundary_problem, 0.0)
+
+#   interior_dofs = [1, 2]
+    boundary_dofs = [3, 4]
+    cass = condensate(fass, boundary_dofs)
+    @test isapprox(full(cass.Kc), [
+        0.0 0.0  0.0  0.0
+        0.0 0.0  0.0  0.0
+        0.0 0.0  4.8 -4.8
+        0.0 0.0 -4.8  4.8])
+   @test isapprox(full(cass.fc)', [0.0 0.0 12.0 12.0])
+   @test cass.interior_dofs == [1, 2]
+
+   x = sparse(zeros(4))'
+   la = sparse(zeros(4))'
+   la[3] = la[4] = 24.0
+   reconstruct!(cass, x)
+   x = full(x)
+   info(la)
+   info(x)
+   @test isapprox(x[1], 1.0)
+   @test isapprox(x[2], 1.0)
+end
+
+end
+
