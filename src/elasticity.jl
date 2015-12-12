a323058ce58f31ac1ba17c436512b7752a9ef2f9
Olli Väinölä <olli.vainola@student.oulu.fi>
2015-12-12 15:22:31 +0200

    implementing ideal plastic material...

--- src/elasticity.jl
+++ src/elasticity.jl
@@ -1,6 +1,8 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
 
+include("vonmises.jl")
+
 # Elasticity problems
 
 abstract ElasticityProblem <: AbstractProblem
@@ -129,6 +131,22 @@ function get_residual_vector{P<:ElasticPlasticProblem}(problem::Problem{P}, elem
 
     # internal forces
     if haskey(element, "youngs modulus") && haskey(element, "poissons ratio")
+        
+        if !haskey(element, "integration points")
+            last_stress = zeros(3,3)
+            last_strain = zeros(3,3)
+        else
+            for each_ip in element("integration points", time)
+                if isapprox(each_ip.xi, ip.xi)
+                    last_stress = ip("stress", time)
+                    last_strain = ip("stress", time)
+                    break
+                end
+            end
+        end
+
+        # last_ip = get_last_ip(problem, element, ip, time)
+        # stress_base = last_ip("stress")
         u = element("displacement", time, variation)
         grad = element(ip, time, Val{:grad})
 #        gradu = element("displacement", ip, time, Val{:grad}, variation)
@@ -139,16 +157,33 @@ function get_residual_vector{P<:ElasticPlasticProblem}(problem::Problem{P}, elem
         young = element("youngs modulus", ip, time)
         poisson = element("poissons ratio", ip, time)
 
+        C = stiffnessTensor(young, poisson)
         mu = young/(2*(1+poisson))
         lambda = young*poisson/((1+poisson)*(1-2*poisson))
         if P == PlaneStressElasticityProblem
             lambda = 2*lambda*mu/(lambda + 2*mu)  # <- correction for 2d problems
         end
-      
-
-        E = 1/2*(F'*F - I)       # large strain
-        #E = 1/2*(gradu + gradu')  # finite strain
-        S = lambda*trace(E)*I + 2*mu*E
+        stress_y = element("yield stress", time).data
+        #E = 1/2*(F'*F - I)        # large strain
+        E = 1/2*(gradu + gradu') # finite strain (total)
+        dstrain = E - last_strain
+        material_model = element("material model", time)
+        s = last_stress
+        de = ForwardDiff.get_value(dstrain)
+        s_v = [s[1,1], s[2,2], s[3,3], s[2,3], s[1,3], s[1,2]]
+        de_ = [de[1,1], de[2,2], de[3,3], de[2,3], de[1,3], de[1,2]]
+        #println("stress: ", s_v)
+        #println("de: ",  de_)
+        #println(C)
+        #println("yield stress: ", stress_y)
+        plastic_multiplier = calculate_stress!(de_, s_v, C, stress_y, Val{:vonMises})
+        # dep = lambda * dfds(s)
+        # upate_material_parameters!(...)
+        S = [s_v[1] s_v[6] s_v[5];
+             s_v[6] s_v[2] s_v[4];
+             s_v[5] s_v[4] s_v[3]]
+        # S = C * (E - dep)
+        #S = lambda*trace(E)*I + 2*mu*E
 
         #J = det(element, ip, time)
         #T = J^-1*F*S*F'
