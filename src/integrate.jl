69ebba5e1142ad34c86304875b6146d24fa3e915
Jukka Aho <jukka.aho@kapsi.fi>
2016-02-05 22:22:40 +0200

    more integration rules

--- src/integrate.jl
+++ src/integrate.jl
@@ -119,6 +119,13 @@ function get_integration_points(::Type{Quad4}, ::Type{Val{2}})
     ]
 end
 
+function get_integration_points(::Type{Quad4}, ::Type{Val{3}})
+    p = [-sqrt(3/5), 0.0, sqrt(3/5)]
+    w = [5/9, 8/9, 5/9]
+    pts = vec([IntegrationPoint([p[i], p[j]], w[i]*w[j]) for i=1:3, j=1:3])
+    return pts
+end
+
 function get_integration_points(::Type{Quad4})
     return get_integration_points(Quad4, Val{2})
 end
@@ -128,7 +135,7 @@ end
 
 function get_integration_points(::Type{Hex8}, ::Type{Val{2}})
     p = 1.0/sqrt(3.0)*[-1.0, 1.0]
-    w = 1.0
+    w = [1.0, 1.0]
     return vec([IntegrationPoint([p[i], p[j], p[k]], w) for i=1:2, j=1:2, k=1:2])
 end
 
