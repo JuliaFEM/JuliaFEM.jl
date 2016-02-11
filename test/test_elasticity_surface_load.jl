04f3c0e7053d06c0a55c5ff08d7234798e3fa335
Jukka Aho <jukka.aho@kapsi.fi>
2016-02-11 16:16:15 +0200

    Geometrically nonlinear formulation for 3d.

--- test/test_elasticity_surface_load.jl
+++ test/test_elasticity_surface_load.jl
@@ -2,7 +2,7 @@
 # License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
 
 using JuliaFEM.Test
-using JuliaFEM.Core: Node, update!, Quad4, Seg2, Problem, Elasticity, Solver, Dirichlet
+using JuliaFEM.Core: Node, update!, Quad4, Seg2, Hex8, Problem, Elasticity, Solver, Dirichlet
 using JuliaFEM.Preprocess: aster_parse_nodes
 
 @testset "test 2d linear elasticity with surface load" begin
@@ -40,7 +40,7 @@ using JuliaFEM.Preprocess: aster_parse_nodes
     # type, name, dimension, unknown_field_name
     boundary_problem = Problem(Dirichlet, "symmetry boundaries", 2, "displacement")
     push!(boundary_problem, sym13, sym23)
-    
+
     solver = Solver("solve block problem")
     solver.is_linear_system = true # to get linear solution
     push!(solver, elasticity_problem)
@@ -95,87 +95,45 @@ end
 
 @testset "test continuum linear elasticity with surface load" begin
 
-    nodes = JuliaFEM.Preprocess.aster_parse_nodes("""
-    COOR_3D
-    N1          0.0 0.0 0.0
-    N2          1.0 0.0 0.0
-    N3          1.0 1.0 0.0
-    N4          0.0 1.0 0.0
-    N5          0.0 0.0 1.0
-    N6          1.0 0.0 1.0
-    N7          1.0 1.0 1.0
-    N8          0.0 1.0 1.0
-    FINSF
-    """)
+    nodes = Dict{Int64, Node}(
+    1 => [0.0, 0.0, 0.0],
+    2 => [1.0, 0.0, 0.0],
+    3 => [1.0, 1.0, 0.0],
+    4 => [0.0, 1.0, 0.0],
+    5 => [0.0, 0.0, 1.0],
+    6 => [1.0, 0.0, 1.0],
+    7 => [1.0, 1.0, 1.0],
+    8 => [0.0, 1.0, 1.0])
 
-    function set_geometry!(element, nodes)
-        element["geometry"] = Vector{Float64}[nodes[i] for i in get_connectivity(element)]
-    end
     element1 = Hex8([1, 2, 3, 4, 5, 6, 7, 8])
-    set_geometry!(element1, nodes)
-    element1["youngs modulus"] = 900.0
-    element1["poissons ratio"] = 0.25
-    element1["displacement"] = (0.0 => Vector{Float64}[[0.0, 0.0, 0.0] for i=1:8])
-
     element2 = Quad4([5, 6, 7, 8])
-    set_geometry!(element2, nodes)
-    element2["displacement traction force"] = Vector{Float64}[[0.0, 0.0, -100.0] for i=1:4]
-    element2["displacement"] = (0.0 => Vector{Float64}[[0.0, 0.0, 0.0] for i=1:4])
+    update!([element1, element2], "geometry", nodes)
+    update!([element1], "youngs modulus", 900.0)
+    update!([element1], "poissons ratio", 0.25)
+    update!([element2], "displacement traction force", Vector{Float64}[[0.0, 0.0, -100.0] for i=1:4])
 
-    problem = ElasticityProblem()
-    push!(problem, element1)
-    push!(problem, element2)
+    elasticity_problem = Problem(Elasticity, "solve continuum block", 3)
+    push!(elasticity_problem, element1)
+    push!(elasticity_problem, element2)
 
-    free_dofs = zeros(Bool, 8, 3)
-    x = 1
-    y = 2
-    z = 3
-    free_dofs[2, x] = true
-    free_dofs[3, [x, y]] = true
-    free_dofs[4, y] = true
-    free_dofs[5, z] = true
-    free_dofs[6, [x, z]] = true
-    free_dofs[7, [x, y, z]] = true
-    free_dofs[8, [y, z]] = true
-    free_dofs = find(vec(free_dofs'))
-    info("free dofs: $free_dofs")
+    symxy = Quad4([1, 2, 3, 4])
+    symxz = Quad4([1, 2, 6, 5])
+    symyz = Quad4([1, 4, 8, 5])
+    update!([symxy, symxz, symyz], "geometry", nodes)
+    symxy["displacement 3"] = 0.0
+    symxz["displacement 2"] = 0.0
+    symyz["displacement 1"] = 0.0
+    boundary_problem = Problem(Dirichlet, "symmetry boundary conditions", 3, "displacement")
+    push!(boundary_problem, symxy, symxz, symyz)
 
-    info("initial force vector")
-    ass = JuliaFEM.Core.assemble(problem, 0.0)
-    info(reshape(full(ass.force_vector), 3, 8))
-    info("initial stiffness matrix")
-    dump(round(Int, full(ass.stiffness_matrix))[free_dofs, free_dofs])
-    solve!(problem, free_dofs, 0.0; max_iterations=10)
-
-#=
-    dx = Quad4([1, 4, 8, 5])
-    dx["displacement 1"] = 0.0
-    dy = Quad4([1, 5, 6, 2])
-    dy["displacement 2"] = 0.0
-    dz = Quad4([1, 2, 3, 4])
-    dz["displacement 3"] = 0.0
-    bc = DirichletProblem("displacement", 3)
-    for el in [dx, dy, dz]
-        set_geometry!(el, nodes)
-        push!(bc, el)
-    end
-    solver = JuliaFEM.Core.DirectSolver()
-    push!(solver, problem)
-    push!(solver, bc)
-    solver.dump_matrices = true
-    solver.name = "3d_hex8"
-    solver(0.0)
-=#
+    solver = Solver("solve 3d block")
+    push!(solver, elasticity_problem)
+    push!(solver, boundary_problem)
+    call(solver)
 
     disp = element1("displacement", [1.0, 1.0, 1.0], 0.0)
     info("displacement at tip: $disp")
-    info("displacement on element: ")
-    for (i, d) in enumerate(element1("displacement", 0.0))
-        @printf "%d % f % f % f\n" [i;d]...
-    end
     # verified using Code Aster.
     # 2015-12-12-continuum-elasticity/vim c3d_grot_gdep_traction_force.comm
     @test isapprox(disp, [3.17431158889468E-02, 3.17431158889468E-02, -1.38591518927826E-01])
-    #@test isapprox(disp, [2.80559539222183E-03, 2.80559539222183E-03, -1.13019918093242E-02])
 end
-
