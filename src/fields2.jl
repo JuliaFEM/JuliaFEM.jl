fce033cdd572d242db6ef10e5d78fccf85e56d89
Jukka Aho <jukka.aho@kapsi.fi>
2016-02-13 01:18:17 +0200

    removed empty files

--- src/fields2.jl
+++
@@ -1,6 +0,0 @@
-# This file is a part of JuliaFEM.
-# License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
-
-# https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/notebooks/2015-06-14-data-structures.ipynb
-
-
