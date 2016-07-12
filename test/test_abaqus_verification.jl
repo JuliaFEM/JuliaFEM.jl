9c1562fff5592432422c6187291ec6ba583e08b0
Jukka Aho <jukka.aho@kapsi.fi>
2016-07-12 06:30:42 +0300

    more abaqus inp reading

--- test/test_abaqus_verification.jl
+++ test/test_abaqus_verification.jl
@@ -18,4 +18,17 @@ end
 @testset "ec38sfs2" begin
     abaqus_run_test("ec38sfs2"; print_test_file=true) || return
     results = abaqus_read_results("ec38sfs2")
+    side = get_results(results, "SECTION"; name="side")
+    @test isapprox(side["SOFM"], 3464.0)
+    @test isapprox(side["SOF1"], 2000.0)
+    @test isapprox(side["SOF2"], 2000.0)
+    @test isapprox(side["SOF3"], 2000.0)
+    @test isapprox(side["SOMM"], 2828.0)
+    @test isapprox(side["SOM1"], 0.0)
+    @test isapprox(side["SOM2"], 2000.0)
+    @test isapprox(side["SOM3"], -2000.0)
+    @test isapprox(side["SOAREA"], 2.000)
+    @test isapprox(side["SOCF1"], 2/3)
+    @test isapprox(side["SOCF2"], 2/3)
+    @test isapprox(side["SOCF3"], 1/6)
 end
