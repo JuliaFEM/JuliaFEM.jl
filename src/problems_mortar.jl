34b8149ab6221bd0cbe4de6591b9706925364107
Jukka Aho <jukka.aho@kapsi.fi>
2016-12-15 09:18:15 +0200

    a new strategy to deal with quadratic elements, change order of surface elements to linear

--- src/problems_mortar.jl
+++ src/problems_mortar.jl
@@ -1,6 +1,31 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
 
+"""
+Parameters
+----------
+dimension
+    dimension of surface, 1 for 2d problems (plane strain, plane stress,
+    axisymmetric) and 2 for 3d problems. It not given, try to determine problem
+    dimension from first element
+rotate_normals
+    if all surface elements are in cw order instead of ccw, this can be used to
+    swap normal directions so that normals point to outward of body
+adjust
+    for elasticity problems only; closes any gaps between surfaces if found
+dual_basis
+    use bi-orthogonal basis when interpolating Lagrange multiplier space
+use_forwarddiff
+    use forwarddiff to linearize contact constraints directly from weighted
+    gap function
+distval
+    charasteristic measure, contact pairs with distance over this value are
+    skipped from contact segmentation algorithm
+linear_surface_elements
+    convert quadratic surface elements to linear elements on the fly
+store_fields
+    not used
+"""
 type Mortar <: BoundaryProblem
     dimension :: Int
     rotate_normals :: Bool
@@ -8,12 +33,13 @@ type Mortar <: BoundaryProblem
     dual_basis :: Bool
     use_forwarddiff :: Bool
     distval :: Float64
+    linear_surface_elements :: Bool
     store_fields :: Vector{Symbol}
 end
 
 function Mortar()
     default_fields = []
-    return Mortar(-1, false, false, false, false, Inf, default_fields)
+    return Mortar(-1, false, false, false, false, Inf, true, default_fields)
 end
 
 function get_unknown_field_name(problem::Problem{Mortar})
