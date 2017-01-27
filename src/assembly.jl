fcf21329e835619b3e0a2ef8dd5af26117ce3f8c
Jukka Aho <jukka.aho@kapsi.fi>
2017-01-27 12:45:13 +0200

    Removed unused code

--- src/assembly.jl
+++ src/assembly.jl
@@ -1,46 +1,6 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
 
-function optimize!(assembly::Assembly)
-    optimize!(assembly.K)
-    optimize!(assembly.Kg)
-    optimize!(assembly.f)
-    optimize!(assembly.fg)
-    optimize!(assembly.C1)
-    optimize!(assembly.C2)
-    optimize!(assembly.D)
-    optimize!(assembly.g)
-    optimize!(assembly.c)
-end
-
-function append!(assembly::Assembly, sub_assembly::Assembly)
-    append!(assembly.M, sub_assembly.M)
-    append!(assembly.K, sub_assembly.K)
-    append!(assembly.Kg, sub_assembly.Kg)
-    append!(assembly.f, sub_assembly.f)
-    append!(assembly.fg, sub_assembly.fg)
-    append!(assembly.C1, sub_assembly.C1)
-    append!(assembly.C2, sub_assembly.C2)
-    append!(assembly.D, sub_assembly.D)
-    append!(assembly.g, sub_assembly.g)
-    append!(assembly.c, sub_assembly.c)
-end
-
-""" Calculate norm of assembly, i.e., norm of each block of matrix. """
-function norm(assembly::Assembly, p=2)
-    N1 = norm(assembly.M, p)
-    N2 = norm(assembly.K, p)
-    N3 = norm(assembly.Kg, p)
-    N4 = norm(assembly.f, p)
-    N5 = norm(assembly.fg, p)
-    N6 = norm(assembly.C1, p)
-    N7 = norm(assembly.C2, p)
-    N8 = norm(assembly.D, p)
-    N9 = norm(assembly.g, p)
-    N10 = norm(assembly.c, p)
-    return [N1, N2, N3, N4, N5, N6, N7, N8, N9, N10]
-end
-
 function isapprox(a1::Assembly, a2::Assembly)
     T = isapprox(a1.K, a2.K)
     T &= isapprox(a1.C1, a2.C1)
