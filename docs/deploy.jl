8a92b45882c20ae53fd90841535aec982e42b2e8
Jukka Aho <ahojukka5@gmail.com>
2017-07-19 07:34:38 +0300

    Set up documentation + lint (#121)
    - remove some automatically generated stuff not should even be in
    repository
    - set up lint + Documents.jl in same way it is defined in freshly started projects
    - add lint + doctest to after_success so that build pass, these needs to be fixed later
    - build is failing on nightly (0.7) but it's ok for release (0.5.2)
    - Documenter.jl supports doctests, so this closes least #23
    - build system is now on Travis-CI completely, so this closes also #68

---
+++ docs/deploy.jl
@@ -0,0 +1,9 @@
+# This file is a part of JuliaFEM.
+# License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE
+
+using Documenter
+using JuliaFEM
+
+deploydocs(
+    deps = Deps.pip("mkdocs", "python-markdown-math"),
+    repo = "github.com/JuliaFEM/JuliaFEM.jl.git")
