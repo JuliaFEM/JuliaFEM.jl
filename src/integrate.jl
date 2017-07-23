e31aae82cb14cac2c805b0050cdb647200d0bf77
Jukka Aho <ahojukka5@gmail.com>
2017-07-23 15:12:46 +0300

    add higher order too than 1

--- src/integrate.jl
+++ src/integrate.jl
@@ -3,45 +3,49 @@
 
 using FEMQuad: get_quadrature_points
 
-# default number of integration points for each element
-# first rule is the default
-# some times we want to increase integration order, e.g. when integrating mass
-# matrix or boundary conditions, so we add additional scheme to do so and that's
-# the second rule
+# Default number of integration points for each element. First rule is the
+# default integration rule returned by `get_integration_points(element)`.
+# Sometimes we want to increase integration order, e.g. when integrating mass
+# matrix or boundary conditions. For that reason, additional rules are provied
+# in list, so e.g. `get_integration_points(element, 1)` returns the second rule,
+# `get_integration_points(element, 2)` third rule and so on. Rules should be
+# ordered so that picking next one integrates more accurately.
 integration_rule_mapping = (
-    :Seg2    => (:GLSEG1,  :GLSEG2),
-    :Seg3    => (:GLSEG2,  :GLSEG3),
-    :NSeg    => (:GLSEG2,  :GLSEG3),
-    :Quad4   => (:GLQUAD1, :GLQUAD4),
-    :Quad8   => (:GLQUAD4, :GLQUAD9),
-    :Quad9   => (:GLQUAD4, :GLQUAD9),
-    :NSurf   => (:GLQUAD4, :GLQUAD9),
-    :Hex8    => (:GLHEX1,  :GLHEX27),
-    :Hex20   => (:GLHEX27, :GLHEX81),
-    :Hex27   => (:GLHEX27, :GLHEX81),
-    :NSolid  => (:GLHEX27, :GLHEX81),
-    :Tri3    => (:GLTRI1,  :GLTRI3),
-    :Tri6    => (:GLTRI3,  :GLTRI4),
-    :Tri7    => (:GLTRI3,  :GLTRI4),
-    :Tet4    => (:GLTET1,  :GTET4),
-    :Tet10   => (:GLTET4,  :GLTET5),
-    :Pyr5    => (:GLPYR5,  ),
-    :Wedge6  => (:GLWED6,  :GLWED21),
+    :Seg2    => (:GLSEG1, :GLSEG2, :GLSEG3, :GLSEG4, :GLSEG5),
+    :Seg3    => (:GLSEG2, :GLSEG3, :GLSEG4, :GLSEG5),
+    :NSeg    => (:GLSEG2, :GLSEG3, :GLSEG4, :GLSEG5),
+    :Quad4   => (:GLQUAD1, :GLQUAD4, :GLQUAD9, :GLQUAD16, :GLQUAD25),
+    :Quad8   => (:GLQUAD4, :GLQUAD9, :GLQUAD16, :GLQUAD25),
+    :Quad9   => (:GLQUAD4, :GLQUAD9, :GLQUAD16, :GLQUAD25),
+    :NSurf   => (:GLQUAD4, :GLQUAD9, :GLQUAD16, :GLQUAD25),
+    :Hex8    => (:GLHEX1, :GLHEX27, :GLHEX81, :GLHEX243),
+    :Hex20   => (:GLHEX27, :GLHEX81, :GLHEX243),
+    :Hex27   => (:GLHEX27, :GLHEX81, :GLHEX243),
+    :NSolid  => (:GLHEX27, :GLHEX81, :GLHEX243),
+    :Tri3    => (:GLTRI1, :GLTRI3, :GLTRI4, :GLTRI6, :GLTRI7, :GLTRI12),
+    :Tri6    => (:GLTRI3, :GLTRI4, :GLTRI6, :GLTRI7, :GLTRI12),
+    :Tri7    => (:GLTRI3, :GLTRI4, :GLTRI6, :GLTRI7, :GLTRI12),
+    :Tet4    => (:GLTET1, :GTET4, :GLTET5, :GLTET15),
+    :Tet10   => (:GLTET4, :GLTET5, :GLTET15),
+    :Pyr5    => (:GLPYR5, ),
+    :Wedge6  => (:GLWED6, :GLWED21),
     :Wedge15 => (:GLWED21, ))
 
 for (E, R) in integration_rule_mapping
-    P = Val{first(R)}
-    code = quote
-        function get_integration_points(element::$E)
-            return get_quadrature_points($P)
-        end
-    end
-    eval(code)
-    if length(R) > 1
-        P = Val{(R[2])}
-        code = quote
-            function get_integration_points(element::$E, ::Type{Val{1}})
-                return get_quadrature_points($P)
+    for i in 1:length(R)
+        P = Val{R[i]}
+        order = Val{i-1}
+        if i == 1
+            code = quote
+                function get_integration_points(element::$E)
+                    return get_quadrature_points($P)
+                end
+            end
+        else
+            code = quote
+                function get_integration_points(element::$E, ::Type{$order})
+                    return get_quadrature_points($P)
+                end
             end
         end
         eval(code)
