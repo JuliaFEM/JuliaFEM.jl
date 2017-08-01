cc3491b67e4991a6f7f71cc3a0c001b843750c54
Jukka Aho <ahojukka5@gmail.com>
2017-08-02 01:06:46 +0300

    AbaqusReader.jl tagged, added to REQUIRE

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,6 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
 
-Pkg.clone("https://github.com/JuliaFEM/AbaqusReader.jl.git")
-Pkg.build("AbaqusReader")
 Pkg.clone("https://github.com/JuliaFEM/AsterReader.jl.git")
 Pkg.build("AsterReader")
 Pkg.clone("https://github.com/JuliaFEM/FEMQuad.jl.git")
