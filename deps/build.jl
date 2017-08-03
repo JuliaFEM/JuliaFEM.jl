46b3845e22d9cd72bbf63fbf9c3d68e9c24ac497
Jukka Aho <ahojukka5@gmail.com>
2017-08-03 20:31:52 +0300

    add package dependency AsterReader.jl

--- deps/build.jl
+++
@@ -1,5 +0,0 @@
-# This file is a part of JuliaFEM.
-# License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
-
-Pkg.clone("https://github.com/JuliaFEM/AsterReader.jl.git")
-Pkg.build("AsterReader")
