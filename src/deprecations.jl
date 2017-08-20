fe3daae5878d6c29d25aafa4228ec80cb6035848
Jukka Aho <ahojukka5@gmail.com>
2017-08-20 19:16:23 +0300

    Deprecate assemble!(problem, time, Val{:mass_matrix})
    Use function assemble_mass_matrix!(problem, time)

---
+++ src/deprecations.jl
@@ -0,0 +1,6 @@
+# This file is a part of JuliaFEM.
+# License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
+
+function assemble!(problem::Problem, time, ::Type{Val{:mass_matrix}})
+    assemble_mass_matrix!(problem, time)
+end
