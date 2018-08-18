8e59e17686c8cf5510eefd14b557d8559d5715a4
Jukka Aho <ahojukka5@gmail.com>
2018-08-18 11:49:40 +0300

    Update linear_static.jl
    Some little tweaks, mainly making that filter function more understandable.

--- examples/linear_static.jl
+++ examples/linear_static.jl
@@ -3,9 +3,7 @@
 
 # # JuliaFEM Linear Static Example
 
-
 # ![](linear_static/freecad.png)
-
 
 # ## Preprocessing
 using JuliaFEM
@@ -32,26 +30,29 @@ model_elements = create_elements(mesh, "OTHER")
 update!(model_elements, "youngs modulus", 208.0E3)
 update!(model_elements, "poissons ratio", 0.30)
 update!(model_elements, "density", 7.80E-9)
-JuliaFEM.add_elements!(model, model_elements)
+add_elements!(model, model_elements)
 
-# Let's ingore Seg3 and Tri6 elements using `filter`
+# We can ignore Seg3 and Tri6 elements using `filter` with a special function
+# returning true if element is something else than Seg3 or Tri6:
 
-model.elements = collect(filter(x -> !isa(x, Union{Element{Seg3},
-                                            Element{Tri6}}), model.elements))
+function is_not_Seg3_or_Tri6(element)
+    return !isa(element, Union{Element{Seg3}, Element{Tri6}})
+end
 
+filter!(is_not_Seg3_or_Tri6, model.elements)
 
 # The whole idea of the JuliaFEM input is to be a normal Julia script, where the
 # user can freely define any functions needed to perform the task. Here we
 # define a function, which finds nodes on the given plane yz, xz or xy from the
 # given height.
 
-
-function add_nodes_at_certain_plane_to_node_set!(mesh,name,vector_id,distance,radius=6.0)
+function add_nodes_at_certain_plane_to_node_set!(mesh, name, vector_id, distance, radius=6.0)
     for (node, coords) in mesh.nodes
         if isapprox(coords[vector_id], distance, atol=radius)
-            add_node_to_node_set!(mesh,name,node)
+            add_node_to_node_set!(mesh, name, node)
         end
     end
+    return nothing
 end
 
 # We will find nodes from the xz-plane going through point (0,50,0) or actually
@@ -64,9 +65,9 @@ add_nodes_at_certain_plane_to_node_set!(mesh,:mid_fixed,2,50.0)
 # dot in FreeCAD and now we can search three closest nodes to these coordinates.
 # Those will be added to the same set `:mid_fixed`.
 
-ipoint = find_nearest_nodes(mesh,[165.0, 88.0, 10],3)
+ipoint = find_nearest_nodes(mesh, [165.0, 88.0, 10],3)
 for poi in ipoint
-    add_node_to_node_set!(mesh,:mid_fixed,poi)
+    add_node_to_node_set!(mesh, :mid_fixed, poi)
 end
 
 # The fixed boundary conditions are defined next.
@@ -85,42 +86,47 @@ update!(model_elements, "displacement load 1", 1.0)
 # Finally the ´Analysis` couples everything togeter.
 analysis = Analysis(Linear, model, fixed)
 
-# Let's request xdmf resuls
+# Let's write resuls to Xdmf file
 
-add_results_writer!(analysis, Xdmf("model_results"; overwrite=true))
+xdmf = Xdmf("model_results"; overwrite=true)
+add_results_writer!(analysis, xdmf)
 
 # Now we have all we need to run the analysis.
 
 run!(analysis)
 
 # ## Postprocessing
+
 # This is how the stresses are requested
-push!(model.postprocess_fields,"stress")
+push!(model.postprocess_fields, "stress")
 
 # Finally let's write the results to the xdmf (this is bug, the solver should
-# do this automatically)
+# do this automatically, see issue #203)
 
 time = 0.0
-JuliaFEM.postprocess!(analysis,time)
-JuliaFEM.write_results!(analysis,time)
+JuliaFEM.postprocess!(analysis, time)
+JuliaFEM.write_results!(analysis, time)
 
 # In order to look the results, we will need to close the xdmf that it is actually
 # written to the file from buffer.
 
-close(analysis.results_writers[1].hdf)
+close(xdmf.hdf)
 
 # Finally when we open the model in ParaView and set some settings we have this
 # end result.
 
-
 # ![](linear_static/paraview.png)
 
-
-
 # ## Testing
+
 # First let's test that we have the output files writen to the disk
 
-using Base.Test
+if VERSION < v"1.0.0"
+    using Base.Test
+else
+    using Test
+end
+
 @test isfile("model_results.xmf")
 @test isfile("model_results.h5")
 
@@ -131,5 +137,3 @@ time = 0.0
 u = analysis("displacement", time)
 u_norms = Dict(i => norm(j) for (i, j) in u)
 @test isapprox(maximum(values(u_norms)),2.4052929896922337)
-
-
