414de67114ba15615b571a50d29676cb0fee4e99
Jukka Aho <ahojukka5@gmail.com>
2025-11-20 17:56:08 +0200

    refactor(backend): Remove Vec{3} conversion in CPU backend
    - Changed 'ξ = Vec{3}(ip.ξ)' to 'ξ = ip.ξ' in compute_element_stiffness
    - No conversion needed since ip.ξ is now already Vec{3}

--- src/backend/cpu.jl
+++ src/backend/cpu.jl
@@ -142,7 +142,7 @@ function compute_element_stiffness(element::Element{N,NIP,F,B}, time::Float64) w
 
     # Integrate over element
     for ip in ips
-        ξ = Vec{3}(ip.ξ)
+        ξ = ip.ξ
         w = ip.weight
 
         # NEW API: Basis function derivatives (shape function gradients in reference coords)
