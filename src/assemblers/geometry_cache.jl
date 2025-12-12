1f6ec9e7fe40c76f8679a3941f24118904845c84
Jukka Aho <ahojukka5@gmail.com>
2025-12-12 23:30:00 +0200

    fix(assemblers): use coords field instead of ξ in geometry cache
    Update geometry cache to use coords field from QuadraturePoint
    instead of deprecated ξ field.
    
    - Change ip.ξ to ip.coords in create_geometry_cache function
    - Update both gradient computation and detJ*w computation

--- src/assemblers/geometry_cache.jl
+++ src/assemblers/geometry_cache.jl
@@ -195,7 +195,7 @@ function create_geometry_cache(
     # Compute gradients and weights at all integration points
     ∇N_data_tuple = ntuple(NIP) do q
         ip = ips[q]
-        ξ = ip.ξ
+        ξ = ip.coords
         w = ip.weight
 
         # Compute Jacobian and physical gradients
@@ -220,7 +220,7 @@ function create_geometry_cache(
     # Compute detJ * weight
     detJ_w_tuple = ntuple(NIP) do q
         ip = ips[q]
-        ξ = ip.ξ
+        ξ = ip.coords
         w = ip.weight
 
         # Recompute Jacobian (could optimize by storing from above)
