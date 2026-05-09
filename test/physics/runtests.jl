b981c8301b87dcb67c4378cc393ff040e5a794af
Jukka Aho <ahojukka5@gmail.com>
2026-05-09 18:46:27 +0300

    test(physics): narrow runner to strain helpers while layer stabilizes
    Legacy physics API tests were retired with the module reset; keep only the strain
    smoke file until higher-level physics coverage returns.
    
    - Import `JuliaFEM` explicitly for includes under `Pkg.test`.

--- test/physics/runtests.jl
+++ test/physics/runtests.jl
@@ -1,16 +1,6 @@
-# This file is a part of JuliaFEM.
-# License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
-
-"""
-Physics module test suite.
-
-Run all physics-related unit tests.
-"""
-
 using Test
+using JuliaFEM
 
-@testset "Physics Module" begin
-    include("test_types.jl")
-    include("test_boundary_conditions.jl")
-    include("test_validation.jl")
+@testset "Physics" begin
+    include("test_strain.jl")
 end
